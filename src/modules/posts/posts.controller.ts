198382dd8b4e476e3414e74fd5c66976fc98500f
Naveez Munir <naveezmunir@gmail.com>
2024-12-27 20:17:52 +0500

    added validator packages

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post as HttpPost } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { Post } from './interfaces/post.interface';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+  @HttpPost()
+  createPost(@Body() createPostDto: CreatePostDto): Post {
+    return this.postsService.createPost(createPostDto);
+  }
+
+  @Get()
+  getAllPosts(): Post[] {
+    return this.postsService.getAllPosts();
+  }
+
+}
--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from './interfaces/post.interface';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  private posts: Post[] = [];
+
+  createPost(createPostDto: CreatePostDto): Post {
+    const post: Post = {
+      id: this.posts.length + 1,
+      ...createPostDto,
+      createdAt: new Date()
+    };
+    this.posts.push(post);
+    return post;
+  }
+
+  getAllPosts(): Post[] {
+    return this.posts;
+  }
+
+}
